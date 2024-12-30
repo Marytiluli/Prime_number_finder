05b3519994c6d4ce1cc883b43134a8bb763a5100
rehemathomas <marytiluli@gmail.com>
2024-12-30 13:37:51 +0300

    Check if the limit is less than 2

--- prime number.cpp
+++ prime number.cpp
@@ -4,3 +4,8 @@ using namespace std;
 
 // Function to generate prime numbers up to a given limit using the Sieve of Eratosthenes
 void generatePrimes(int limit) {
+	    if (limit < 2) {
+        cout << "No prime numbers available up to " << limit << "." << endl;
+        return;
+    }
+
