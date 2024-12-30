c06b1e236fd08ea573962991b68c424cf52a127f
rehemathomas <marytiluli@gmail.com>
2024-12-30 13:45:19 +0300

    Initialize the boolean vector

--- prime number.cpp
+++ prime number.cpp
@@ -4,8 +4,12 @@ using namespace std;
 
 // Function to generate prime numbers up to a given limit using the Sieve of Eratosthenes
 void generatePrimes(int limit) {
-	    if (limit < 2) {
+	if (limit < 2) {
         cout << "No prime numbers available up to " << limit << "." << endl;
         return;
     }
+    
+    vector<bool> isPrime(limit + 1, true); // Initialize a boolean vector
+    isPrime[0] = isPrime[1] = false;       // 0 and 1 are not prime numbers
+
 
