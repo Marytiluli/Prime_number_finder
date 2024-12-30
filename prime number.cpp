ed34367f41c6c4f45da7554cb329e18299b59272
rehemathomas <marytiluli@gmail.com>
2024-12-30 13:36:31 +0300

    Function definition starts

--- prime number.cpp
+++ prime number.cpp
@@ -1,3 +1,6 @@
 #include <iostream>
 #include <vector>
 using namespace std;
+
+// Function to generate prime numbers up to a given limit using the Sieve of Eratosthenes
+void generatePrimes(int limit) {
